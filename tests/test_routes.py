5c85cc0607c6ddd1d879c47e246eccdbe6ecdd31
Vinicius Gomes Quaglia <viniciusquaglio@gmail.com>
2024-09-12 05:28:38 -0400

    Read account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -130,3 +130,13 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
